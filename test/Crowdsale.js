df2d08b8b6ff2f1e972c7be51a2d8c1fd0246647
Joseph Reynolds <72422816+JosephJamesReynolds@users.noreply.github.com>
2023-12-12 20:57:31 -0800

    1.Create Contract

--- test/Crowdsale.js
+++ test/Crowdsale.js
@@ -10,13 +10,20 @@ describe('Crowdsale', () => {
 
 	beforeEach(async () => {
 		const Crowdsale = await ethers.getContractFactory('Crowdsale')
-		crowdsale = await Crowdsale.deploy()
+		const Token = await ethers.getContractFactory('Token')
 
+		token = await Token.deploy('Joseph Money', 'JMN', '1000000')
+
+		crowdsale = await Crowdsale.deploy(token.address)
 	})
 
 	describe('Deployment', () => {
 		it('has correct name', async () => {
 			expect(await crowdsale.name()).to.eq("Crowdsale")
+		})
+
+		it('returns token address', async () => {
+			expect(await crowdsale.token()).to.equal(token.address)
 		})
 	})
 })
