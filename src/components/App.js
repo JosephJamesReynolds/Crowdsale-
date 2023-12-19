71f590e81d674c91d29a85f72cc356c690d4216b
Joseph Reynolds <72422816+JosephJamesReynolds@users.noreply.github.com>
2023-12-19 14:33:18 -0800

    Navbar

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import { Container } from 'react-bootstrap';
+
+// Components
+import Navigation from './Navigation';
+
+function App() {
+	return(
+		<Container>
+		<Navigation />
+		</Container>
+  )
+}
+
+export default App;
