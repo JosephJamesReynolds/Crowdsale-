0e4fb9843021011bbd8b38302eb6250e950a3ecc
Joseph Reynolds <72422816+JosephJamesReynolds@users.noreply.github.com>
2023-12-19 16:16:45 -0800

    Load Account

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,43 @@
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import { useEffect, useState } from 'react'
 import { Container } from 'react-bootstrap';
+import { ethers } from 'ethers'
 
 // Components
 import Navigation from './Navigation';
+import Info from './Info';
+
 
 function App() {
+
+	const [account, setAccount] = useState(null)
+
+	// account -> Variable of current account value
+	// setAccount('0x0...') -> Function to update account value
+	// null is default value
+
+	const loadBlockchainData = async () => {
+		const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+		const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+		const account = ethers.utils.getAddress(accounts[0])
+		setAccount(account)
+
+		// Add to state
+	}
+
+	useEffect(() => {
+		loadBlockchainData()
+	});
+
+
+
 	return(
 		<Container>
-		<Navigation />
+			<Navigation />
+			<hr />
+			{account && (
+			<Info account={account} />
+			)}
 		</Container>
   )
 }
