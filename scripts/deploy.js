e7fca21011b24a7b60b85afdfdd8d4882d39b6b9
Joseph Reynolds <72422816+JosephJamesReynolds@users.noreply.github.com>
2024-01-04 20:52:50 -0800

    Crowdsale Complete

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,31 +7,38 @@
 const hre = require("hardhat");
 
 async function main() {
-  const NAME = 'Joseph Money'
-  const SYMBOL = 'JMN'
-  const MAX_SUPPLY = '1000000'
-  const PRICE = ethers.utils.parseUnits('0.025', 'ether')
+  const NAME = "Joseph Money";
+  const SYMBOL = "JMN";
+  const MAX_SUPPLY = "1000000";
+  const PRICE = ethers.utils.parseUnits("0.025", "ether");
 
   // Deploy token
-  const Token = await hre.ethers.getContractFactory('Token')
-  let token = await Token.deploy(NAME, SYMBOL, MAX_SUPPLY)
+  const Token = await hre.ethers.getContractFactory("Token");
+  let token = await Token.deploy(NAME, SYMBOL, MAX_SUPPLY);
 
-  await token.deployed()
-  console.log(`Token deployed to: ${token.address}\n`)
+  await token.deployed();
+  console.log(`Token deployed to: ${token.address}\n`);
 
   // Deploy crowdsale
-  const Crowdsale = await hre.ethers.getContractFactory('Crowdsale')
-  const crowdsale = await Crowdsale.deploy(token.address, PRICE, ethers.utils.parseUnits(MAX_SUPPLY, 'ether'))
-  await crowdsale.deployed()
+  const Crowdsale = await hre.ethers.getContractFactory("Crowdsale");
+  const crowdsale = await Crowdsale.deploy(
+    token.address,
+    PRICE,
+    ethers.utils.parseUnits(MAX_SUPPLY, "ether")
+  );
+  await crowdsale.deployed();
 
-  console.log(`Crowdsale deployed to: ${crowdsale.address}\n`)
+  console.log(`Crowdsale deployed to: ${crowdsale.address}\n`);
 
   // send tokens to crowdsale
 
-  const transaction = await token.transfer(crowdsale.address, ethers.utils.parseUnits(MAX_SUPPLY, 'ether'))
-  await transaction.wait()
+  const transaction = await token.transfer(
+    crowdsale.address,
+    ethers.utils.parseUnits(MAX_SUPPLY, "ether")
+  );
+  await transaction.wait();
 
-  console.log(`Tokens transferred to Crowdsale\n`)  
+  console.log(`Tokens transferred to Crowdsale\n`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- src/components/App.js
+++ src/components/App.js
@@ -1,102 +1,119 @@
-import { useEffect, useState } from 'react'
-import { Container } from 'react-bootstrap'
-import { ethers } from 'ethers'
+import { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import { ethers } from "ethers";
 
 // Components
-import Navigation from './Navigation';
-import Info from './Info';
-import Loading from './Loading';
-import Progress from './Progress';
-
+import Navigation from "./Navigation";
+import Info from "./Info";
+import Loading from "./Loading";
+import Progress from "./Progress";
+import Buy from "./Buy";
 
 // ABIs
-import TOKEN_ABI from '../abis/Token.json'
-import CROWDSALE_ABI from '../abis/Crowdsale.json'
+import TOKEN_ABI from "../abis/Token.json";
+import CROWDSALE_ABI from "../abis/Crowdsale.json";
 
-// Config 
-import config from '../config.json';
-
+// Config
+import config from "../config.json";
 
 function App() {
-	const [provider, setProvider] = useState(null)
-	const [crowdsale, setCrowdsale] = useState(null)
+  const [provider, setProvider] = useState(null);
+  const [crowdsale, setCrowdsale] = useState(null);
 
-	const [account, setAccount] = useState(null)
-	const [accountBalance, setAccountBalance] = useState(null)
+  const [account, setAccount] = useState(null);
+  const [accountBalance, setAccountBalance] = useState(null);
 
-	const [price, setPrice] = useState(0)
-	const [maxTokens, setMaxTokens] = useState(0)
-	const [tokensSold, setTokensSold] = useState(0)
+  const [price, setPrice] = useState(0);
+  const [maxTokens, setMaxTokens] = useState(0);
+  const [tokensSold, setTokensSold] = useState(0);
 
+  const [isLoading, setIsLoading] = useState(true);
 
-	const[isLoading, setIsLoading] = useState(true)
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
 
-	const loadBlockchainData = async () => {
+    // Fetch Chain Id
+    const { chainId } = await provider.getNetwork();
 
-		const provider = new ethers.providers.Web3Provider(window.ethereum)
-		setProvider(provider)
+    // Initiate contracts
+    const token = new ethers.Contract(
+      config[chainId].token.address,
+      TOKEN_ABI,
+      provider
+    );
+    const crowdsale = new ethers.Contract(
+      config[chainId].crowdsale.address,
+      CROWDSALE_ABI,
+      provider
+    );
+    setCrowdsale(crowdsale);
 
-		// Initiate contracts
-		const token = new ethers.Contract(config[31337].token.address, TOKEN_ABI, provider)
-		const crowdsale = new ethers.Contract(config[31337].crowdsale.address, CROWDSALE_ABI, provider)
-		setCrowdsale(crowdsale)
+    // Fetch account
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    const account = ethers.utils.getAddress(accounts[0]);
+    setAccount(account);
 
-		// Fetch account
-		const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-		const account = ethers.utils.getAddress(accounts[0])
-		setAccount(account)
+    // Fetch account balance
+    const accountBalance = ethers.utils.formatUnits(
+      await token.balanceOf(account),
+      18
+    );
+    setAccountBalance(accountBalance);
 
-		// Fetch account balance 
-		const accountBalance = ethers.utils.formatUnits(await token.balanceOf(account), 18)
-		setAccountBalance(accountBalance)
+    // Fetch price
+    const price = ethers.utils.formatUnits(await crowdsale.price());
+    setPrice(price);
 
-		// Fetch price
-		const price = ethers.utils.formatUnits(await crowdsale.price())
-		setPrice(price)
+    // Fetch max tokens
+    const maxTokens = ethers.utils.formatUnits(await crowdsale.maxTokens());
+    setMaxTokens(maxTokens);
 
-		// Fetch max tokens
-		const maxTokens = ethers.utils.formatUnits(await crowdsale.maxTokens())
-		setMaxTokens(maxTokens)
+    //Fetch tokens sold
+    const tokensSold = ethers.utils.formatUnits(await crowdsale.tokensSold());
+    setTokensSold(tokensSold);
 
-		//Fetch tokens sold
-		const tokensSold = ethers.utils.formatUnits(await crowdsale.tokensSold())
-		setTokensSold(tokensSold)
+    setIsLoading(false);
 
-		setIsLoading(false)
+    // Add to state
+  };
 
-		// Add to state
-	}
+  useEffect(() => {
+    if (isLoading) {
+      loadBlockchainData();
+    }
+  }, [isLoading]);
 
-	useEffect(() => {
-		if (isLoading) {
-		loadBlockchainData()
-		}
-	}, [isLoading]);
+  return (
+    <Container>
+      <Navigation />
 
+      <h1 className="my-4 text-center">Introducing Joseph Money!</h1>
 
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <p className="text-center">
+            <strong>Current Price:</strong> {price} ETH
+          </p>
+          <Buy
+            provider={provider}
+            price={price}
+            crowdsale={crowdsale}
+            setIsLoading={setIsLoading}
+          />
+          <Progress maxTokens={maxTokens} tokensSold={tokensSold} />
+        </>
+      )}
 
-	return(
-		<Container>
-			<Navigation />
+      <hr />
 
-			<h1 className='my-4 text-center'>Introducing Joseph Money!</h1>
-
-			{isLoading ? (
-				<Loading />
-				) : (
-				<>
-				<p className="text-center"><strong>Current Price:</strong> {price} ETH</p>
-				<Progress maxTokens={maxTokens}	tokensSold={tokensSold} />
-				</>
-				)}
-
-			<hr />
-
-			{account && (
-			<Info account={account} accountBalance={accountBalance} />
-			)}
-		</Container>
-  )
+      {account && <Info account={account} accountBalance={accountBalance} />}
+    </Container>
+  );
 }
 
 export default App;
